695b518c42c8c325c97e63fc812fd841a70cf978
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-09-27 14:50:26 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.setAttribute('data-source', item.original);
+  image.alt = item.description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  return listItem;
+}
+
+function openLightbox(event) {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    const source = event.target.dataset.source;
+    const description = event.target.alt;
+    const instance = basicLightbox.create(
+      `<img src="${source}" alt="${description}">`
+    );
+    instance.show();
+  }
+}
+
+galleryContainer.addEventListener('click', openLightbox);
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+galleryContainer.append(...galleryItemsMarkup);
+
+
 console.log(galleryItems);
-
-
-// import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
-
-// instance.show()
